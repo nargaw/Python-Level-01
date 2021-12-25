462997034c7417a58c625e70a804e16e6f7ed352
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2021-12-24 23:28:32 -0500

    Create Hello World.py

---
+++ Hello World.py
@@ -0,0 +1,30 @@
+#Hello World python 3.0
+#strings
+print("strings and things")
+print('hello world')
+print("""multi-line
+    code is written
+    like this""")
+print("this is" + "some code")
+
+#print new line
+print('\n')
+
+#Math
+print("Math")
+print(40 + 40) #add
+print(50 - 45) #sub
+print(50 * 80) #mult
+print(30 / 60) #div
+print(2 ** 2) #exponenets
+print(40 % 13) #modulo - remaining
+print(50 // 6) #number without leftovers
+
+#variables & methods
+print('\n')
+print('variables')
+quote = "apples and bananas"
+print(len(quote)) #return length
+print(quote.upper()) #turn to uppercase
+print(quote.lower()) #turn to lowercase
+print(quote.title()) #capitalize first letter of each word
